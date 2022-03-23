9b45e99b204052a1a8dae6c9320d62109b1636c3
deepthipavanbh <101954977+deepthipavanbh@users.noreply.github.com>
2022-03-23 09:06:09 +0530

    Create Addition.java
    it performs addition of two numbers

---
+++ Addition.java
@@ -0,0 +1,23 @@
+
+
+class Addition
+{  
+  /*int a,b;
+  Addition(int a,int b)
+  {
+    a = this.a;
+    b= this.b;
+    
+  }*/
+  
+  public static void main(String args[])
+  {
+    void int(int a,int b)
+    {
+      return a+b;
+    }
+    
+  }
+    
+    
+}
